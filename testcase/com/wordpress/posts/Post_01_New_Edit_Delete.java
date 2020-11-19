2a05d3210a145b6e11131093b7243e761990916b
buihang <buihang230491@gmail.com>
2020-11-19 23:02:57 +0700

    Innit automation framework

---
+++ testcase/com/wordpress/posts/Post_01_New_Edit_Delete.java
@@ -0,0 +1,36 @@
+package com.wordpress.posts;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Post_01_New_Edit_Delete {
+  @BeforeClass
+  public void beforeClass() {
+  }
+	
+  @Test
+  public void TC_01_NewPost() {
+  }
+  
+  @Test
+  public void TC_02_ViewPost() {
+  }
+  
+  @Test
+  public void TC_03_EditPost() {
+  }
+  
+  @Test
+  public void TC_04_Make_Post_Private() {
+  }
+  
+  @Test
+  public void TC_05_DeletePost() {
+  }
+  
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
---
+++ testdata/com/wordpress/data/Posts.java
@@ -0,0 +1,15 @@
+package com.wordpress.data;
+
+public class Posts {
+	public class NewPost{
+	
+	}
+	
+	public class EditPost{
+		
+	}
+	
+	public class CopyPost{
+		
+	}
+}
